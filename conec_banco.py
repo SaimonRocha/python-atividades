719dae8d3b6906673df98ec10c76d4d67ed11503
Saimon A.S Rocha <82518570+SaimonRocha@users.noreply.github.com>
2022-12-01 14:12:59 -0300

    Update conec_banco.py

--- conec_banco.py
+++ conec_banco.py
@@ -1,21 +1,32 @@
+# Esse programa ao ser executado ele irá salvar no seu banco de dados
+# As cidades para que o projeto funcione
+# Para isso tenha o Pyhton no seu PC e execute o py nome_projeto
+# Não esqueça de ter o banco de dados conectado
+
 # Importamos a biblioteca:
-import pymysql
- 
+import json     #Abertura de Json
+
+#Função para remover os caracteres indesejados
+def chr_remove(old, to_remove):
+    new_string = old
+    for x in to_remove:
+        new_string = new_string.replace(x, '')
+    return new_string
+
 # Abrimos uma conexão com o banco de dados:
-conexao = pymysql.connect(db='python', user='root', passwd='')
- 
+conexao = psycopg2.connect(host="localhost", database="crud", user="postgres", password="123")
+
 # Cria um cursor:
 cursor = conexao.cursor()
- 
-# Executa o comando:
-email = input("Digite seu email: ")
-senha = input("Digite sua senha: ")
 
-cursor.execute("ALTER TABLE cadastro AUTO_INCREMENT = 1")
-cursor.execute(f"INSERT INTO cadastro (email, senha) VALUES ('{email}', '{senha}')");
+if(conexao):
+    print('Conectado com Sucesso!')
+else:
+    print('Erro ao abrir conexão')
+
 
 # Efetua um commit no banco de dados.
 conexao.commit()
- 
+
 # Finaliza a conexão
 conexao.close()
